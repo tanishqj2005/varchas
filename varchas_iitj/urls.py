be47fcfe66183b8583eef8a0ca1b7aac06c13def
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-01-17 02:42:40 +0530

    changed settings, updated create team

--- varchas_iitj/urls.py
+++ varchas_iitj/urls.py
@@ -32,8 +32,7 @@ urlpatterns = [
     path('sponsors/', include('sponsors.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
