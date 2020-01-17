9a59121d2290f5033558b79ae2f202de6cbcdb56
killbotxd <sonawane.1@iitj.ac.in>
2020-01-17 06:40:42 +0530

    add our team url

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView, NavBarSubOptionsPageView
+from .views import IndexView, NavBarSubOptionsPageView, OurTeamView
 
 app_name = 'main'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
+    path('OurTeam', OurTeamView.as_view(), name='OurTeam'),
     path('<slug:slug>', NavBarSubOptionsPageView.as_view(), name='navbarsuboptionpage'),
 ]
