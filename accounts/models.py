e6e3b5ed360454341befacbc3e573028117fe1f8
killbotXD <sonawane.1@iitj.ac.in>
2020-02-26 02:07:14 +0530

    change accommodation type

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,10 @@ from events.models import Event
 
 class UserProfile(models.Model):
     # choices
+    ACCOMMODATION_CHOICES = (
+        ('N', 'No'),
+        ('Y', 'Yes'),
+    )
     GENDER_CHOICES = (
         ('M', 'Male'),
         ('F', 'Female'),
@@ -65,7 +69,7 @@ class UserProfile(models.Model):
         max_length=1, choices=GENDER_CHOICES, default='M')
     college = models.CharField(max_length=128)
     state = models.CharField(max_length=2, choices=STATE_CHOICES)
-    accomodation_required = models.BooleanField(default=False)
+    accommodation_required = models.CharField(max_length=1, choices=ACCOMMODATION_CHOICES)
     accomodation_type = models.CharField(max_length=1, default=1)
     amount_required = models.PositiveSmallIntegerField(default=0, blank=True)
     amount_paid = models.PositiveSmallIntegerField(default=0, blank=True)
