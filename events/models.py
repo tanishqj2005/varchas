7e4379d1629c570dc8d88a5ff004aab94fc068bd
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-03-08 11:03:38 +0530

    changed model for matches

--- events/models.py
+++ events/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.signals import post_save
 # from django.shortcuts import reverse
 from adminportal.models import AdminProfile
 # from .utils import unique_slug_generator
@@ -39,15 +38,14 @@ class Event(models.Model):
 
 # pre_save.connect(event_pre_save_receiver, sender=Event)
 
-# !!FOR SPORTS EVENTS!!
 
-class match(Event):
+class Match(Event):
     team1 = models.ForeignKey(TeamRegistration, on_delete=models.CASCADE, related_name="team1")
     team2 = models.ForeignKey(TeamRegistration, on_delete=models.CASCADE, related_name="team2")
     STATUS_CHOICES = (
-        ('1', 'live'),
-        ('2', 'notstarted'),
-        ('3', 'ended'),
+        ('1', 'Live'),
+        ('2', 'Not Started'),
+        ('3', 'Ended'),
     )
     GAME_CHOICES = (
         ('1', 'Athletics'),
@@ -66,9 +64,9 @@ class match(Event):
        ('3', 'SEMI FINAL'),
        ('4', 'FINAL'),
     )
-    game = models.CharField(max_length=50, choices=GAME_CHOICES)
-    match_type = models.CharField(max_length=50, choices=MATCH_CHOICES)
-    play_status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='2')
+    game = models.CharField(max_length=2, choices=GAME_CHOICES)
+    match_type = models.CharField(max_length=2, choices=MATCH_CHOICES)
+    play_status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='2')
     end_comment = models.TextField(blank=True, null=True)
     winner = models.ForeignKey(TeamRegistration, on_delete=models.CASCADE, related_name="winner", blank=True, null=True)
     final_score = models.CharField(max_length=50, blank=True, null=True)
@@ -76,27 +74,9 @@ class match(Event):
     def __str__(self):
         return self.event_id
 
-    def get_model(self):
-        if self.game == '5':
-            obj = CrickeLogs
-        elif self.game == '3' or self.game == '6':
-            obj = FootballLogs
-        else:
-            obj = VolleyballLogs
-        return obj
 
-
-def match_post_save_receiver(sender, instance, *args, **kwargs):
-    if instance.play_status == '2':
-        obj = instance.get_model()
-        obj(match=instance).save()
-
-
-post_save.connect(match_post_save_receiver, sender=match)
-
-
-class CrickeLogs(models.Model):
-    match = models.ForeignKey(match, on_delete=models.CASCADE, related_name="match")
+class CricketLogs(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name="match")
     run1 = models.IntegerField(default=0)
     run2 = models.IntegerField(default=0)
     wicket1 = models.IntegerField(default=0)
@@ -104,33 +84,24 @@ class CrickeLogs(models.Model):
     overs1 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
     overs2 = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
 
-    def __str__(self):
-        return "cricket"
-
     def score(self):
         return [str(self.run1) + "/" + str(self.wicket1), str(self.run2) + "/" + str(self.wicket2)]
 
 
 class FootballLogs(models.Model):
-    match = models.ForeignKey(match, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
     score1 = models.IntegerField(default=0)
     score2 = models.IntegerField(default=0)
-
-    def __str__(self):
-        return "football"
 
     def score(self):
         return [self.score1, self.score2]
 
 
 class VolleyballLogs(models.Model):
-    match = models.ForeignKey(match, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
     score1 = models.CharField(max_length=50, null=True, default='0')
     score2 = models.CharField(max_length=50, null=True, default='0')
     setNo = models.SmallIntegerField(default=1)
-
-    def __str__(self):
-        return "volleyball"
 
     def score(self):
         return [self.score1, self.score2, self.setNo]
---
+++ events/views.py
@@ -0,0 +1,6 @@
+# from django.views.generic import CreateView
+# from .forms import MatchForm
+
+
+# class CreateMatch(CreateView):
+# form_class = MatchForm
