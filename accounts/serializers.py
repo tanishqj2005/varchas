248501cf06d97b8ba1222cc1ca00c0eb273f48f2
Kaustubh Dixit <kdkishu335@gmail.com>
2020-03-25 17:44:12 +0530

    Removed flake error from serializer.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="accounts:user-detail")
+
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
