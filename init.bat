818fc922eef519c1664659b675623442a3de1d94
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-06-14 03:08:32 +0530

    removed unused files

--- init.bat
+++
@@ -1,9 +0,0 @@
-git pull upstream master
-python manage.py makemigrations main
-python manage.py makemigrations accounts
-python manage.py makemigrations adminportal
-python manage.py makemigrations events
-python manage.py makemigrations registration
-python manage.py makemigrations sponsors
-python manage.py migrate
-python manage.py runserver
